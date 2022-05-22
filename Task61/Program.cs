d51d943d9acff35434e0339f6bca5cb5cf5799af
ashvarvar <ashvarvar@gmail.com>
2022-05-22 22:04:43 +0300

    done

---
+++ Task61/Program.cs
@@ -0,0 +1,43 @@
+﻿//Дополнительная задача 61: Вывести первые N строк треугольника Паскаля. 
+//Сделать вывод в виде равнобедренного треугольника
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+ 
+namespace PascalTriangle
+{
+ 
+    class PascalTriangle
+    {
+        static void Main(string[] args)
+        {
+ 
+           
+            Console.Write("Введите количество строк : ");
+            string input = Console.ReadLine();
+ 
+            int n = Convert.ToInt32(input);
+ 
+ 
+            for (int y = 0; y < n; y++)
+            {
+                int c = 1;
+                for (int q = 0; q < n - y; q++)
+                {
+                    Console.Write("   ");
+                }
+ 
+                for (int x = 0; x <= y; x++)
+                {
+                    Console.Write("   {0:D} ", c);
+                    c = c * (y - x) / (x + 1);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
+          
